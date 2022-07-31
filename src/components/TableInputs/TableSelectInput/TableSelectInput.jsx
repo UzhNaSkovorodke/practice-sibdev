e0787be6a1e0f06b8a04c291998179a92426276d
UzhNaSkovorodke <piratt.kiri@gmail.com>
2022-08-01 00:52:15 +0700

    feature/#3-popup-designs-lastFix

--- src/components/TableInputs/TableSelectInput/TableSelectInput.jsx
+++ src/components/TableInputs/TableSelectInput/TableSelectInput.jsx
@@ -1,47 +1,46 @@
-import { ArrowIcon } from 'src/assets/icons';
-import { defaultInputStyles } from 'src/components/DefaultInput/DefaultInput';
-import { classnames } from 'src/utils';
+import { useState } from 'react';
 
-import { defaultTableInputStyles } from '../TableDefaultInput/TableDefaultInput';
+import { ArrowIcon } from 'src/assets/icons';
+// import { defaultInputStyles } from 'src/components/DefaultInput/DefaultInput';
+// import { classnames } from 'src/utils';
+
+// import { defaultTableInputStyles } from '../TableDefaultInput/TableDefaultInput';
 
 import styles from './TableSelectInput.module.scss';
 
 const TableSelectInput = ({ value, isError, emptyListText, placeholder, selectOptions, onChange }) => {
-  let isOpenoptionWrapper = true;
-  const settValue = (text, value) => {
-    document.getElementById('myButton').innerText = text;
-    document.getElementById('myButton').value = value;
-  };
+  const [isShow, setisShow] = useState(false);
+  const [chosenOption, setchosenOption] = useState();
 
-  const setoptionWrapper = () => {
-    if (isOpenoptionWrapper === true) {
-      document.getElementById('optionWrapper').style.display = 'none';
-      isOpenoptionWrapper = false;
-    } else {
-      document.getElementById('optionWrapper').style.display = 'block';
-      isOpenoptionWrapper = true;
-    }
+  const toggleIsOpen = () => {
+    isShow === true ? setisShow(false) : setisShow(true);
   };
-
   const changeCategory = (event) => {
-    onChange(event.target.id);
-    settValue(event.target.innerText, event.target.value);
-    setoptionWrapper();
+    setchosenOption(event.target.textContent);
+    setisShow(!isShow);
+    onChange(event);
   };
-
   return (
     <div className={styles.inputWrapper}>
       <ArrowIcon className={styles.input__icon} />
-      <button id="myButton" value={value} className={styles.myButton} onClick={setoptionWrapper}>
-        {placeholder}
+      <button
+        value={chosenOption}
+        disabled={!selectOptions.length}
+        id="myButton"
+        className={styles.myButton}
+        onClick={toggleIsOpen}
+      >
+        {chosenOption || placeholder}
       </button>
-      <ul id="optionWrapper" className={styles.optionWrapper}>
-        {selectOptions.map((item, index) => (
-          <li key={index} value={item.id} id={item.id} onClick={changeCategory} className={styles.choosedOption}>
-            {item.name}
-          </li>
-        ))}
-      </ul>
+      {isShow && (
+        <ul id="optionWrapper" className={styles.optionWrapper}>
+          {selectOptions.map((item, index) => (
+            <li key={index} value={item.id} onClick={changeCategory} className={styles.choosedOption}>
+              {item.name}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
